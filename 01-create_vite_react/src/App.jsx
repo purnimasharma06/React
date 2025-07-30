a850eaa88aefb2318dc0a7a382bdeca447bf046a
purnimasharma06 <purnima.sharma3773@gmail.com>
2025-07-30 07:33:47 +0530

    First Commit

---
+++ 01-create_vite_react/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import Chai from "./chai"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1> 
+    {/*
+    we write the evaluated expression or the final outcome in the curly braces 
+    */}
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
