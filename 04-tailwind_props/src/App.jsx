9029673696696696e8bd0bfee21777a7cbaf35c5
purnimasharma06 <purnima.sharma3773@gmail.com>
2025-07-31 19:21:02 +0530

    First Commit

---
+++ 04-tailwind_props/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css';
+import Card from './components/Card';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind </h1>
+   <Card username = "Purnima" btnText='About Purnima '/>
+   <Card username = "Chai aur Code"/>
+    </>
+  )
+}
+
+export default App
---
+++ 04-tailwind_props/src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Card = ({ username, btnText ="visit me" }) => {
+    console.log(username);
+//   const username = "John Doe";
+//   const btnText = "Read More";
+
+
+  return (
+    <div className="relative h-[400px] w-[300px] rounded-md">
+      <img
+        src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+        alt="AirMax Pro"
+        className="z-0 h-full w-full rounded-md object-cover"
+      />
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-left">
+        <h1 className="text-lg font-semibold text-white">{username}</h1>
+        <p className="mt-2 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+          debitis?
+        </p>
+        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+          {btnText} →
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
