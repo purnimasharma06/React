5531933d94d3acd554e19f0074a1c8830d3c0dbc
purnimasharma06 <purnima.sharma3773@gmail.com>
2025-08-13 23:32:21 +0530

    first commit

---
+++ 12-reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import {createSlice, nanoid } from '@reduxjs/toolkit';
+//nanoid -> unique id generate karta hai 
+const initialState = {
+    todos: [{id: 1, text: "Hello world"}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        // addtodo = redux toolkit me syntax hai (state, action) pass karte hai 
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
